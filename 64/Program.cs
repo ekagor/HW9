be694921d75b4d8d0096840b99b2874dd37c7fd9
boris.kondrashov <suborg@mail.ru>
2022-11-16 23:39:10 +0500

    final

---
+++ 64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все чётные числа в 
+// промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+
+PrintInfo();
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+PrintEvenNumbersInterval(number);
+
+void PrintInfo(){
+    Console.WriteLine("Программа выводит все чётные числа в промежутке от N до 1");
+    Console.WriteLine("Выполнено с помощью рекурсии");
+}
+
+void PrintEvenNumbersInterval(int number){
+    if (number % 2 != 0)
+    number = number-1;
+    if (number == 0)
+    return;
+Console.Write($"{number}, ");
+PrintEvenNumbersInterval(number - 2);  
+}
---
+++ 66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+PrintInfo();
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+int sum = 0;
+Console.Write($"Сумма натуральных элементов в промежутке от {numberM} до {numberN} равна ");
+SumNaturalNumbers(numberM,numberN,sum);
+
+void PrintInfo(){
+    Console.WriteLine("Программа находит сумму натуральных элементов ");
+    Console.WriteLine("в промежутке от M до N. Выполнено с помощью рекурсии");
+}
+
+void SumNaturalNumbers(int numberM, int numberN, int sum){
+    if (numberM > numberN){
+        Console.WriteLine($"{sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    SumNaturalNumbers(numberM, numberN, sum);
+}
